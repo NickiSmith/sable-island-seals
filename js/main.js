69371a895236510fdb9b77872913452f40edf87c
Nicki Smith <jnsmith22@wisc.edu>
2019-11-03 13:57:56 -0500

    New interaction added
    Layer control added: stylized basemap and imagery. Imagery source may not be the best.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,25 @@
     Final Project - Sable Island Seals
 */
 
-//initialize the map and set the view
-var mymap = L.map('mapid').setView([43.929898, -59.906358], 13);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+var stylized = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	maxZoom: 19
-}).addTo(mymap);
+	maxZoom: 17
+}),
+    imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 17
+});
+
+var mymap = L.map('mapid', {
+    center: [43.929898, -59.906358],
+    zoom: 13,
+    layers: [stylized, imagery]
+});
+
+var basemaps = {
+    "Stylized": stylized,
+    "Imagery": imagery
+};
+
+L.control.layers(basemaps).addTo(mymap);
