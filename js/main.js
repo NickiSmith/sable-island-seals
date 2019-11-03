a2b820638273e2d5669ff8a2cedf8453504630e7
Nicki Smith <jnsmith22@wisc.edu>
2019-11-03 10:01:32 -0500

    Layout updates
    Created a basic layout with some divs and added a leaflet map. Currently only one basemap.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
     GEOG 575 Fall 2019
     Final Project - Sable Island Seals
 */
+
+//initialize the map and set the view
+var mymap = L.map('mapid').setView([43.929898, -59.906358], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(mymap);
