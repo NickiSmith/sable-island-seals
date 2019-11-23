90cabade4e8ea8900d36c062c041bec0d2f85811
Nicki Smith <jnsmith22@wisc.edu>
2019-11-23 14:55:01 -0500

    Updates
    Added seal locator markers, removed obsolete data files, added image carousel, updated path animations.  Also added new map tileset (not functional yet.)

--- js/main.js
+++ js/main.js
@@ -7,12 +7,21 @@
 //----- create leaflet map -----//
 //------------------------------//
 
+
 var stylized = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 17
-}),
-    imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+})
+
+// -- custom tileset with seal data baked in --//
+// -- possibly needs to be fixed on the tileset publishing side --//
+/*var stylized = L.esri.tiledMapLayer({
+  url: 'https://uw-mad.maps.arcgis.com/home/item.html?id=d4b7706885a84a0ca90c8ebdbbae5bf9#overview',
+  maxZoom: 15
+});*/
+
+var imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
     maxZoom: 17
 });
@@ -29,8 +38,21 @@ var basemaps = {
     
 };
 
-L.control.layers(basemaps).addTo(mymap);
+//----- create seal markers ------ //
+//-- this could be updated to load from csv if we choose --//
+var spot1=L.marker([44.01598,-59.699775]).bindPopup('You found seals!'),
+    spot2=L.marker([44.005652,-59.717613]).bindPopup('You found seals!'),
+    spot3=L.marker([43.981472,-59.753418]).bindPopup('You found seals!'), spot4=L.marker([43.979207,-59.753689]).bindPopup('You found seals!'), spot5=L.marker([43.935239,-60.063347]).bindPopup('You found seals!'), spot6=L.marker([43.935989,-60.049944]).bindPopup('You found seals!'), spot7=L.marker([43.955967,-59.804487]).bindPopup('You found seals!'), spot8=L.marker([43.973698,-59.765904]).bindPopup('You found seals!'), spot9=L.marker([43.977625,-59.759687]).bindPopup('You found seals!');
 
+// -- create layer group and overlay for seal markers -- //
+var sealSpots = L.layerGroup([spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8, spot9]);
+
+var spotterOverlay = {
+    "Seal Spotter": sealSpots
+}
+
+// ------- add basemaps and spotter overlay layers to map and layer control ----------- //
+L.control.layers(basemaps, spotterOverlay).addTo(mymap);
 
 //-----------------------------------------
 //--- leaflet overlay pane script below ---
@@ -40,7 +62,8 @@ L.control.layers(basemaps).addTo(mymap);
 var svgOver = d3.select(mymap.getPanes().overlayPane).append("svg"),
     g = svgOver.append("g").attr("class", "pane");
 
-//load park boundary polygon
+//load park boundary polygon?
+//this might be unecessary because it's just the shape of the island...
 var parkData="data/NPboundary.geojson"
 
 //load geojson seal data for seal paths
@@ -52,8 +75,9 @@ var loadData = d3.json(sealData, function(error, collection) {
         this.stream.point(point.x, point.y);
     }
     
-    var transform = d3.geoTransform({point: projectPoint}),
-        path = d3.geoPath().projection(transform);
+    var transform = d3.geoTransform({point: projectPoint})
+    
+    var path = d3.geoPath().projection(transform);
     
     // create paths
     var feature = g.selectAll("path")
@@ -63,7 +87,9 @@ var loadData = d3.json(sealData, function(error, collection) {
     .attr("class", function(d){
         return d.properties.ID;
     })
+    .attr("opacity", "0")
     .attr("d", path);
+
     
     //resets svg when map is panned
     mymap.on("moveend", reset);
@@ -85,11 +111,12 @@ var loadData = d3.json(sealData, function(error, collection) {
           .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
         
         feature.attr("d", path);
-    }    
-
+    }
 
     //----- line animation adapted from http://bl.ocks.org/fryford/2925ecf70ac9d9b51031 -----//
     //---- animate paths on click -------//
+    
+//Still working on how best to visualize these paths
     
     //--- seal#1 F104 ---//
     d3.select("#seal1").on("click", function(d, i) {
@@ -103,13 +130,14 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".F104").node().getTotalLength();
     
         d3.select(".F104")
-        // Set the line pattern to be a long line  followed by an equally long gap
-        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        // Set the line pattern to be a line followed by a gap
+        .attr("stroke-dasharray", "100" + " " + totalLength)
         // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
         // Then the following lines transition the line so that the gap is hidden...
+        .style("stroke-width", "1")
         .transition()
-        .duration(100000)
+        .duration(80000)
         .ease(d3.easeLinear)
         .attr("stroke-dashoffset", 0)
         //.style("opacity", "1");
@@ -128,7 +156,9 @@ var loadData = d3.json(sealData, function(error, collection) {
     
         d3.select(".F532")
         // Set the line pattern to be a long line  followed by an equally long gap
-        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        //.attr("stroke-dasharray", totalLength + " " + totalLength)
+        // Set the line pattern to be a line followed by a gap
+        .attr("stroke-dasharray", "100" + " " + totalLength)
         // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
         // Then the following lines transition the line so that the gap is hidden...
@@ -151,8 +181,8 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".K88").node().getTotalLength();
     
         d3.select(".K88")
-        // Set the line pattern to be a long line  followed by an equally long gap
-        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        // Set the line pattern to be a line followed by a gap
+        .attr("stroke-dasharray", "100" + " " + totalLength)
         // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
         // Then the following lines transition the line so that the gap is hidden...
@@ -175,8 +205,8 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".S0749").node().getTotalLength();
     
         d3.select(".S0749")
-        // Set the line pattern to be a long line  followed by an equally long gap
-        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        // Set the line pattern to be a line followed by a gap
+        .attr("stroke-dasharray", "100" + " " + totalLength)
         // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
         // Then the following lines transition the line so that the gap is hidden...
@@ -199,8 +229,8 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".S0758").node().getTotalLength();
     
         d3.select(".S0758")
-        // Set the line pattern to be a long line  followed by an equally long gap
-        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        // Set the line pattern to be a line followed by a gap
+        .attr("stroke-dasharray", "100" + " " + totalLength)
         // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
         // Then the following lines transition the line so that the gap is hidden...
@@ -289,34 +319,8 @@ window.onload = setMap();
                 .attr("r",5)
                 .attr("transform", function(d) {return "translate(" + projection(point) + ")";})
                 .attr("r", "3px")
-                .attr("fill", "#49556f")
+                .attr("fill", "#8BA5D9")
                 .attr("stroke", "#49556f");
-
-/* adding label experimentation -------------
-            //add text label for point
-            //    .attr("r", "5px")
-              //  .attr("fill", "#303140")
-                //.attr("stroke", "#49556f");
-            
-            //add text label for point (still not working)
-            locatorMap.selectAll("text")
-            .data(point)
-            .enter()
-            .append("text")
-            .attr("transform", function(d) {return "translate(" + projection(point) + ")";});
-            
-            //Add the text attributes
-            var textLabels = text
-                 .attr("font-family", "sans-serif")
-                 .attr("font-size", "20px")
-                 .attr("fill", "black");
-
-            .attr("class", "label")
-            //.attr("height", "10px")
-            .attr("transform", function(d) {return "translate(" + projection(point) + ")";})
-            .html(label);
-            
-            */
             
         } //end of the callback() function
     } //end of setMap()
@@ -357,6 +361,14 @@ mymap.on('click', onMapClick);
     $("#seal4").html("<img class='buttonSVG' src='img/seal4.svg'>");
     $("#seal5").html("<img class='buttonSVG' src='img/seal5.svg'>");
 
+
+//----------------------------------//
+//Slideshow
+//----------------------------------//
+
+
+
+
 //-----------------------------------------------
 /*---------------- Resources --------------------
 //-----------------------------------------------
@@ -370,6 +382,12 @@ Animating the line
 http://bl.ocks.org/fryford/2925ecf70ac9d9b51031
 
 https://stackoverflow.com/questions/28682454/moving-a-circle-along-a-d3-path-animating-at-varying-speeds
+
+stroke-dasharray
+https://bl.ocks.org/mbostock/5649592
+
+point-along-path
+https://bl.ocks.org/mbostock/1705868
 
 */
 
