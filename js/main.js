0a9ade87e79859286b7e6e551f295c90d1cc4009
Nicki Smith <jnsmith22@wisc.edu>
2019-12-02 12:04:14 -0500

    Updates
    Styling updates, animation updates, citation updates.

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ var basemaps = {
     
 };
 
-// ------ seal tileset ------//
+// ------ add seal tileset ------//
 var sealtiles = L.esri.tiledMapLayer({
   url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/greyseals_tiles/MapServer',
   maxZoom: 15
@@ -38,7 +38,6 @@ var sealtiles = L.esri.tiledMapLayer({
 
 
 //----- create seal markers ------ //
-
 var sealIcon = L.icon({
     iconUrl: 'img/seal-icon.png',
     iconSize: [40, 40], // size of the icon
@@ -48,34 +47,51 @@ var spot1=L.marker([44.01598,-59.699775], {icon: sealIcon}).bindPopup('You found
     spot2=L.marker([44.005652,-59.717613], {icon: sealIcon}).bindPopup('You found seals!'),
     spot3=L.marker([43.981472,-59.753418], {icon: sealIcon}).bindPopup('You found seals!'), spot4=L.marker([43.979207,-59.753689],{icon: sealIcon}).bindPopup('You found seals!'), spot5=L.marker([43.935239,-60.063347],{icon: sealIcon}).bindPopup('You found seals!'), spot6=L.marker([43.935989,-60.049944],{icon: sealIcon}).bindPopup('You found seals!'), spot7=L.marker([43.955967,-59.804487],{icon: sealIcon}).bindPopup('You found seals!'), spot8=L.marker([43.973698,-59.765904],{icon: sealIcon}).bindPopup('You found seals!'), spot9=L.marker([43.977625,-59.759687],{icon: sealIcon}).bindPopup('You found seals!');
 
+
 // -- create layer group and overlay for seal markers -- //
 var sealSpots = L.layerGroup([spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8, spot9]);
 
-// -- add basemaps and spotter overlay layers to map and layer control -- //
 
+// -- add basemaps and spotter overlay layers to map and layer control -- //
 var spotterOverlay = {
     "Seal Spotter": sealSpots,
-    "Seal Tracking": sealtiles
+    "Seal Tracking": sealtiles,
 }
+
 L.control.layers(basemaps, spotterOverlay).addTo(mymap);
 
- //----------- easy button -----------//
 
-var sealSpotterPopup = L.popup().setContent('<h1 id="spotterTitle"><strong>Welcome, Seal Spotters!  </strong><img src="img/seal-icon.png" height="40px" width="55px"></h1><br><p id="spotterText">Ready to play? Choose the imagery map using the layers button in the upper right, then explore the map to see how many seals you can find on the island.<br><br>Need a hint? Turn on the "Seal Spotter" layer using the same button and zoom in on the seal icons to see colonies basking on the sand.</p>');
+ //------- create easy button -------//
+var sealSpotterPopup = L.popup().setContent('<h1 id="spotterTitle"><strong>Welcome, Seal Spotters!  </strong><img src="img/seal-icon.png" height="40px" width="45px"></h1><br><p id="spotterText">Ready to play? Choose the imagery map using the layers button in the upper right, then explore the map to see how many seals you can find on the island.<br><br>Need a hint? Turn on the "Seal Spotter" layer using the same button and zoom in on the seal icons to see colonies basking on the sand.</p>');
 
-L.easyButton( '<img src="img/seal-icon.png" height="25px" width="23px">', function(btn, mymap){
+L.easyButton( '<img src="img/seal-icon.png" height="25px" width="20px">', function(btn, mymap){
   sealSpotterPopup.setLatLng(mymap.getCenter()).openOn(mymap);
 }).addTo(mymap);
+
+
+// -------- add legend to the map -------//
+var imageUrl = 'img/legend4.svg';
+var imageBounds = [[43.551823, -63.485459], [43.13635,-62.327289]];
+var legend = L.imageOverlay(imageUrl, imageBounds);
+		legend.addTo(mymap);
+
+
+// -------- add logo to the map -------//
+var imageUrl = 'img/logo3.svg';
+var imageBounds = [[43.551823,-58.203021], [43.13635,-57.539005]];
+var logo = L.imageOverlay(imageUrl, imageBounds);
+logo.addTo(mymap);
+
 
 //-----------------------------------------//
 //--- leaflet overlay pane script below ---//
 //-----------------------------------------//
 
-//create overlay pane
+//--- create overlay pane ---//
 var svgOver = d3.select(mymap.getPanes().overlayPane).append("svg"),
     g = svgOver.append("g").attr("class", "pane");
 
-//load geojson seal data for seal paths
+//-- load geojson seal data for seal paths --//
 var sealData="data/allSeals.geojson";
 var loadData = d3.json(sealData, function(error, collection) {
     if (error) throw error;
@@ -83,12 +99,10 @@ var loadData = d3.json(sealData, function(error, collection) {
         var point = mymap.latLngToLayerPoint(new L.LatLng(y, x));
         this.stream.point(point.x, point.y);
     }
-    
     var transform = d3.geoTransform({point: projectPoint})
-    
     var path = d3.geoPath().projection(transform);
     
-    // create paths
+    //--- create paths ---//
     var feature = g.selectAll("path")
     .data(collection.features)
     .enter()
@@ -100,7 +114,7 @@ var loadData = d3.json(sealData, function(error, collection) {
     .attr("d", path);
 
     
-    //resets svg when map is panned
+    //-- resets svg when map is panned --//
     mymap.on("moveend", reset);
     reset();
     function reset(){  
@@ -115,15 +129,14 @@ var loadData = d3.json(sealData, function(error, collection) {
           .style("top", topLeft[1] + "px")
           .style("background", "none")
           .style("opacity", "1");
-
         g
           .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-        
         feature.attr("d", path);
     }
 
-    //----- line animation adapted from http://bl.ocks.org/fryford/2925ecf70ac9d9b51031 -----//
-    //---- animate paths on click -------//
+    //------------------------//
+    //---- path animation ----//
+    //------------------------//
     
     //--- seal#1 F104 ---//
     d3.select("#seal1").on("click", function(d, i) {
@@ -133,25 +146,21 @@ var loadData = d3.json(sealData, function(error, collection) {
         d3.select(".S0749").style("opacity", "0")
         d3.select(".S0758").style("opacity", "0")
         
-        // Determine the total length of the line 
-        var totalLength =   d3.select(".F104").node().getTotalLength();
+        // get the length of the path 
+        var totalLength = d3.select(".F104").node().getTotalLength();
         
-        //might be able to use this to fix starting and ending point of animation?
-        //length = totalLength + "100";
-    
         d3.select(".F104")
-        // Set the line pattern to be a line followed by a gap
+        // use the length of the line to create line pattern
         .attr("stroke-dasharray", "100" + " " + totalLength)
-        // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
-        // Then the following lines transition the line so that the gap is hidden...
-        .style("stroke-width", "2")
-        .transition()
-        .duration(100000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        //.style("opacity", "1");
-        
+        .style("stroke-width", "3")
+        .transition() // animate the line
+            .duration(35000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+        .transition() // use chained transition to remove line after animation
+            .duration(200)
+            .style("opacity", "0"); 
     })
     
     //--- seal#2 F532 ---//
@@ -162,23 +171,19 @@ var loadData = d3.json(sealData, function(error, collection) {
         d3.select(".S0749").style("opacity", "0")
         d3.select(".S0758").style("opacity", "0")
         
-        // Determine the total length of the line 
+        // get the length of the path
         var totalLength =   d3.select(".F532").node().getTotalLength();
-    
         d3.select(".F532")
-        // Set the line pattern to be a long line  followed by an equally long gap
-        //.attr("stroke-dasharray", totalLength + " " + totalLength)
-        // Set the line pattern to be a line followed by a gap
         .attr("stroke-dasharray", "100" + " " + totalLength)
-        // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
-        // Then the following lines transition the line so that the gap is hidden...
-        .style("stroke-width", "2")
-        .transition()
-        .duration(100000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        //.style("opacity", "1");
+        .style("stroke-width", "3")
+        .transition() // animate the line
+            .duration(35000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+        .transition() // use chained transition to remove line after animation
+            .duration(200)
+            .style("opacity", "0");
     })
     
     //--- seal#3 K88 ---//
@@ -193,17 +198,17 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".K88").node().getTotalLength();
     
         d3.select(".K88")
-        // Set the line pattern to be a line followed by a gap
+        // use the length of the line to create line pattern
         .attr("stroke-dasharray", "100" + " " + totalLength)
-        // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
-        // Then the following lines transition the line so that the gap is hidden...
-        .style("stroke-width", "2")
-        .transition()
-        .duration(100000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        //.style("opacity", "1");
+        .style("stroke-width", "3")
+        .transition() // animate the line
+            .duration(35000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+        .transition() // use chained transition to remove line after animation
+            .duration(200)
+            .style("opacity", "0");
     })
     
     //--- seal#4 S0749 ---//
@@ -218,17 +223,17 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".S0749").node().getTotalLength();
     
         d3.select(".S0749")
-        // Set the line pattern to be a line followed by a gap
+        // use the length of the line to create line pattern
         .attr("stroke-dasharray", "100" + " " + totalLength)
-        // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
-        // Then the following lines transition the line so that the gap is hidden...
-        .style("stroke-width", "2")
-        .transition()
-        .duration(100000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        //.style("opacity", "1");
+        .style("stroke-width", "3")
+        .transition() // animate the line
+            .duration(35000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+        .transition() // use chained transition to remove line after animation
+            .duration(200)
+            .style("opacity", "0");
     })
     
     //--- seal#5 S0758 ---//
@@ -243,25 +248,102 @@ var loadData = d3.json(sealData, function(error, collection) {
         var totalLength =   d3.select(".S0758").node().getTotalLength();
     
         d3.select(".S0758")
-        // Set the line pattern to be a line followed by a gap
+        // use the length of the line to create line pattern
         .attr("stroke-dasharray", "100" + " " + totalLength)
-        // Set the intial starting position so that only the gap is shown by offesetting by the total length of the line
         .attr("stroke-dashoffset", totalLength)
-        // Then the following lines transition the line so that the gap is hidden...
-        .style("stroke-width", "2")
-        .transition()
-        .duration(100000)
-        .ease(d3.easeLinear)
-        .attr("stroke-dashoffset", 0)
-        //.style("opacity", "1");
+        .style("stroke-width", "3")
+        .transition() // animate the line
+            .duration(35000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+         .transition() // use chained transition to remove line after animation
+            .duration(200)
+            .style("opacity", "0");
     })
     
-        //--- make all seals visible ---//
-    d3.select("#allSeals").on("click", function(d, i) {
+        //--- animate all seals ---//
+        d3.select("#allSeals").on("click", function(d, i) {
+        
+        d3.selectAll("path").transition().duration(0);
         d3.selectAll("path").style("opacity", "1");
-    })
+ 
+        //-- seal5 --//
+        var totalLength =   d3.select(".S0758").node().getTotalLength();
+        
+        d3.select(".S0758")
+            .attr("stroke-dasharray", "100" + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .style("stroke-width", "3")
+            .transition()
+                .duration(35000)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+            .transition()
+                .duration(200)
+                .style("opacity", "0");
+              
+        //-- seal4 --//
+        var totalLength =   d3.select(".S0749").node().getTotalLength();
     
- //-----  end of line animation -----//    
+        d3.select(".S0749")
+            .attr("stroke-dasharray", "100" + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .style("stroke-width", "3")
+            .transition()
+                .duration(35000)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+            .transition()
+                .duration(200)
+                .style("opacity", "0");         
+            
+        //-- seal3 --//
+        var totalLength =   d3.select(".K88").node().getTotalLength();
+    
+        d3.select(".K88")
+            .attr("stroke-dasharray", "100" + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .style("stroke-width", "3")
+            .transition()
+                .duration(35000)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+            .transition()
+                .duration(200)
+                .style("opacity", "0");       
+        
+        //-- seal2 --//
+        var totalLength =   d3.select(".F532").node().getTotalLength();
+    
+        d3.select(".F532")
+            .attr("stroke-dasharray", "100" + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .style("stroke-width", "3")
+            .transition()
+                .duration(35000)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+            .transition()
+                .duration(200)
+                .style("opacity", "0");       
+            
+        //-- seal1 --//
+        var totalLength =   d3.select(".F104").node().getTotalLength();
+        
+        d3.select(".F104")
+            .attr("stroke-dasharray", "100" + " " + totalLength)
+            .attr("stroke-dashoffset", totalLength)
+            .style("stroke-width", "3")
+            .transition()
+                .duration(35000)
+                .ease(d3.easeLinear)
+                .attr("stroke-dashoffset", 0)
+            .transition()
+                .duration(200)
+                .style("opacity", "0"); 
+        })
+    
+ //-----  end of path animation script -----//    
     
     
 }); 
@@ -324,36 +406,41 @@ window.onload = setMap();
             var point = [-59.914991, 43.926013]
             
             //create label for point
-            //var label = "<p>"+ "Sable Island" + "</p>"
+            var label = "Sable Island"
 
             //add Sable Island marker to svg
             locatorMap.selectAll("circles.points")
                 .data(point)
                 .enter()
                 .append("circle")
-                .attr("r",5)
                 .attr("transform", function(d) {return "translate(" + projection(point) + ")";})
                 .attr("r", "3px")
                 .attr("fill", "#8BA5D9")
                 .attr("stroke", "#49556f");
+         
+            //Add tesxt label to locator map
+            var text = locatorMap.selectAll("text")
+                .data(point)
+                .enter()
+                .append("text");
             
-            
-            
-            function onMapClick(e) {
-                popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(locatorMap);
-            }
+            //Add SVG Text Element Attributes
+            var textLabels = text
+                .attr("x", 185)
+                .attr("y", 173)
+                .text(label)
+                .attr("font-family", "Open Sans Condensed")
+                .attr("font-size", "12px")
+                .attr("fill", "#49556f");
 
-            locatorMap.on('click', onMapClick);
             
         } //end of the callback() function
     } //end of setMap()
+
 //-------- end of locator map script -------//
 
 //--------- locator tool --------------//
-
+/*
 var popup = L.popup();
 
 function onMapClick(e) {
@@ -364,35 +451,20 @@ function onMapClick(e) {
 }
 
 mymap.on('click', onMapClick);
+*/
 
 //---------end locator tool -----------//
 
-// -------- add logo to the map -------//
-		var imageUrl = 'img/logo3.svg';
-		var imageBounds = [[43.551823,-58.203021], [43.13635,-57.539005]];
-		var logo = L.imageOverlay(imageUrl, imageBounds);
-		logo.addTo(mymap);
-
-// -------- end add the logo to the map --------//
-
-
-// -------- add legend to the map -------//
-		var imageUrl = 'img/legend2.svg';
-		var imageBounds = [[43.551823, -63.485459], [43.13635,-62.327289]];
-		var legend = L.imageOverlay(imageUrl, imageBounds);
-		legend.addTo(mymap);
-
-// -------- end add the legned to the map --------//
 
 //----------------------------------//
 //------- Add button icons ---------//
 //----------------------------------//
 
-    $("#seal1").html("<img class='buttonIMG' src='img/seal1.png'>");
-    $("#seal2").html("<img class='buttonIMG' src='img/seal2.png'>");
-    $("#seal3").html("<img class='buttonIMG' src='img/seal3.png'>");
-    $("#seal4").html("<img class='buttonIMG' src='img/seal4.png'>");
-    $("#seal5").html("<img class='buttonIMG' src='img/seal5.png'>");
+$("#seal1").html("<img class='buttonIMG' src='img/seal1.png'>");
+$("#seal2").html("<img class='buttonIMG' src='img/seal2.png'>");
+$("#seal3").html("<img class='buttonIMG' src='img/seal3.png'>");
+$("#seal4").html("<img class='buttonIMG' src='img/seal4.png'>");
+$("#seal5").html("<img class='buttonIMG'    src='img/seal5.png'>");
 
 //-----------------------------------------------//
 /*---------------- Resources --------------------//
@@ -408,6 +480,8 @@ http://bl.ocks.org/fryford/2925ecf70ac9d9b51031
 
 https://stackoverflow.com/questions/28682454/moving-a-circle-along-a-d3-path-animating-at-varying-speeds
 
+https://www.codementor.io/higheredbob/d3-v3-animate-paths-and-points-twieb5blx
+
 stroke-dasharray
 https://bl.ocks.org/mbostock/5649592
 
@@ -417,5 +491,13 @@ https://bl.ocks.org/mbostock/1705868
 Easy button
 https://github.com/CliffCloud/Leaflet.EasyButton
 
+Adding text label to locator
+https://www.dashingd3js.com/svg-text-element
+
+Chaining transitions
+https://bl.ocks.org/d3noob/899a0b2490318a96f9ebd40a5a84e4a7
+
+Restarting transitions
+http://xaedes.de/dev/transitions/
 */
 
